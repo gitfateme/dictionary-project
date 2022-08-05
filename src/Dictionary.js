125af9c20a869b32836439b80edd0ce0b3957c90
FAteme <miss.l3lue@gmail.com>
2022-08-05 15:15:44 +0430

    Added dictionary api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(r) {
+    console.log(r.data[0]);
+  }
+
   function search(e) {
     e.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+
+    e.target[0].value = "";
   }
 
   function handleKeywordChange(e) {
