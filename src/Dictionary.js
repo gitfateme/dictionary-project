0303bfa04b82b7f37d125e60a159f09aabf1b497
FAteme <miss.l3lue@gmail.com>
2022-08-11 18:41:40 +0430

    Added pexels image api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,13 +2,19 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(r) {
     setResults(r.data[0]);
+  }
+
+  function handlePexelsResponse(r) {
+    setPhotos(r.data.photos);
   }
 
   function search(e) {
@@ -19,6 +25,18 @@ export default function Dictionary() {
     axios.get(apiUrl).then(handleResponse);
 
     e.target[0].value = "";
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000186b62cdc32da4b48ac12d6df09ecc314";
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    axios
+      .get(pexelsApiUrl, {
+        headers: {
+          Authorization: `Bearer ${pexelsApiKey}`,
+        },
+      })
+      .then(handlePexelsResponse);
   }
 
   function handleKeywordChange(e) {
@@ -36,6 +54,7 @@ export default function Dictionary() {
       </section>
 
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
